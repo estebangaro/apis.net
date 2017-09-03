943b58cdfcd2b294f630411030f89022470d724b
Esteban GaRo <estebangaro@outlook.com>
2017-09-03 13:57:28 -0500

    Update SignalR Service
    This commit includes a sample of  chat's SignalR service.

---
+++ signalR_services/Configurations/StartupSignalService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(signalR_services.Configurations.StartupSignalService))]
+
+namespace signalR_services.Configurations
+{
+    public class StartupSignalService
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.Map("/Signals/ChatService",
+            //    appBuilder =>
+            //    {
+            //        // Pending enable CORS to accept cross origin requests
+            //        // please download Microsoft.Owin.Cors nuget package.
+            //        app.RunSignalR<signalR_services.chatservice>(
+            //            new Microsoft.AspNet.SignalR.ConnectionConfiguration { EnableJSONP = true });
+            //    }
+            //);
+            app.MapSignalR<signalR_services.chatservice>("/Signals/ChatService");
+        }
+    }
+}
